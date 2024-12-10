8ab5e65b25ba6240d26f7e495f0659f3451eb310
webDesignMumeishon <muma.sanmartin2011@gmail.com>
2024-12-10 07:10:18 -0300

    add padding

--- src/components/LabelInput.tsx
+++ src/components/LabelInput.tsx
@@ -9,7 +9,7 @@ type Props = {
 const LabelInput = ({ children, inputId }: Props) => {
 
     return (
-        <Label htmlFor={inputId} className="text-sm text-slate-50 font-semibold  bg-brand-2 p-1 rounded-xl" >
+        <Label htmlFor={inputId} className="text-sm text-slate-50 font-semibold  bg-brand-2 p-1 rounded-xl px-4" >
             {children}
         </Label>
     )
